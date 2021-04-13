632a818a5a44829a6d8e377107c13eac9f98ecb4
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-13 18:19:10 +0530

    Added AddressBook Array and stored Contacts

--- addressBookJS.js
+++ addressBookJS.js
@@ -1,22 +1,19 @@
-const usrInput = require("readline-sync");
+const userInput = require("readline-sync");
 const fields = ["firstName", "lastName", "address", "city", "state", "zipCode", "phoneNumber", "email"];
 const regexPattern = ["^[A-Z]{1}[a-z]{2,14}$", "^[A-Z]{1}[a-z]{2,14}$", "^[A-Z]{1}[a-z]{3,14}$", "^[A-Z]{1}[a-z]{3,14}$", "^[A-Z]{1}[a-z]{2,14}$", "^[0-9]{6,}", "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$", "^[A-Za-z0-9+-]+(\\.[A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]{2,}){1,2}$"]
+let addressBook = new Array();
 
 var Contact = function (contactDetails) {
     this.contactDetails = contactDetails;
-    this.printContact = function () {
-        console.log(contact);
-    }
 }
 
-let contactDetails = new Array()
-
 function createContact() {
+    let contactDetails = new Array();
     let temp;
     for (var i = 0; i <= fields.length - 1; i++) {
         let input = true;
         while (input) {
-            temp = usrInput.question(`Enter ${fields[i]}: `);
+            temp = userInput.question(`Enter ${fields[i]}: `);
             if (temp.match(regexPattern[i])) {
                 contactDetails[i] = temp;
                 input = false;
@@ -33,9 +30,34 @@ function createContact() {
 
         }
     }
+    let contact = new Contact(contactDetails);
+    console.log(contact);
+    addressBook.push(contact);
 }
 
-createContact();
+function addressBookService() {
+    let userChoice;
+    do {
+        userChoice = userInput.question("\n******Menu*****\nEnter 1: Create new Contact \nEnter 2: To Print Contacts \nEnter 0: To Exit: ");
+        switch (userChoice) {
+            case "1":
+                let userEntry;
+                do {
+                    createContact();
+                    userEntry = userInput.question("Enter 1: To Create more Contacts: \nEnter 2: To go back to menu: ");
+                } while (userEntry == 1);
+                break;
+            case "2":
+                console.log(addressBook);
+                break;
+            case "0":
+                userChoice = 0;
+                break;
+            default:
+                console.log("\nInvalid Input please Enter a valid input: \n");
+        }
+    } while (userChoice != 0);
+    console.log("\nThank You");
+}
 
-let contact = new Contact(contactDetails)
-contact.printContact();
+addressBookService();
